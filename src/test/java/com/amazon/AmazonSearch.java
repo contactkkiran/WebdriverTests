90bbc94372af3f2331afdd886b7a3d01d2008bc5
Kiran <contactkkk@hotmail.com>
2021-02-26 21:17:48 +0530

    Added new chrome driver.exe and modified Amazon search.java file

--- src/test/java/com/amazon/AmazonSearch.java
+++ src/test/java/com/amazon/AmazonSearch.java
@@ -44,28 +44,13 @@ public class AmazonSearch {
 	public void verifySearchButton() throws InterruptedException {
 		// driver = new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		driver.get("http://www.amazon.com");
+		driver.get("https://www.amazon.in");
 		driver.findElement(By.id("twotabsearchtextbox")).sendKeys("cheapest Samsung Galaxy S8");
 		Thread.sleep(5000);
 
 		driver.findElement(
-				By.xpath("//span[@id='nav-search-submit-text']/ following-sibling::input[@class='nav-input']")).click();
-		Thread.sleep(5000);
-		driver.findElement(By.xpath("//h2[contains(text(),'Samsung Galaxy S8 64GB GSM Unlocked Phone')]")).click();
-
-		String price = driver.findElement(By.xpath("//span[@id='priceblock_ourprice']")).getText();
-		// Add to cart
-
-		Thread.sleep(5000);
-
-		driver.findElement(By.id("add-to-cart-button")).click();
-
-		// Click on Add button
-		Thread.sleep(5000);
-
-		driver.findElement(By.id("siAddCoverage-announce")).click();
-
-		System.out.println(price);
-
+				By.xpath("//input[@id='nav-search-submit-button']")).click();
+		
+		
 	}
 }
